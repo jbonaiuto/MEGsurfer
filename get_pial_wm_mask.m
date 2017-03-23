6dc4005f9f1ee2c77dae753799f4ab613b44e181
jbonaiuto <jbonaiuto@gmail.com>
2017-03-23 18:10:36 +0000

    refactored thresholding

--- get_pial_wm_mask.m
+++ get_pial_wm_mask.m
@@ -1,5 +1,5 @@
 function [pial_mask,wm_mask,mask]=get_pial_wm_mask(pial_metric, wm_metric,...
-    threshold, pial_white_map)
+    threshold, pial_white_map, varargin)
 % GET_PIAL_WM_MASK  Create a mask of pial, white matter and combined
 % surface based on threshold
 %
@@ -12,6 +12,19 @@ function [pial_mask,wm_mask,mask]=get_pial_wm_mask(pial_metric, wm_metric,...
 % for the white matter surface(where the data on that surface exceeds the
 % threshold), and one fo the combine surface (where vertices on either
 % surface exceed the threshold).
+%   get_pial_wm_mask(...,'param','value','param','value'...) allows
+%    additional param/value pairs to be used. Allowed parameters:
+%    * thresh_type - 'lower' (default) or 'upper' - whether threshold is an
+%    upper or lower limit
+
+% Parse inputs
+defaults = struct('thresh_type', 'lower');  %define default values
+params = struct(varargin{:});
+for f = fieldnames(defaults)',
+    if ~isfield(params, f{1}),
+        params.(f{1}) = defaults.(f{1});
+    end
+end
 
 % Combined mask
 mask=[];
@@ -20,17 +33,17 @@ pial_mask=[];
 % White matter mask
 wm_mask=[];
 
-% If threshold is not 0
-if abs(threshold)>0
+% If threshold is not []
+if length(threshold)>0
     % Mapped white matter data
     mapped_wm_metric=wm_metric(pial_white_map);
     
     % Find vertices greater than threshold
-    if threshold>0
+    if strcmp(params.thresh_type,'lower')
         % Create pial and white masks and mapped white mask
-        pial_mask=find(pial_metric>=threshold);
-        wm_mask=find(wm_metric>=threshold);
-        mapped_wm_mask=find(mapped_wm_metric>=threshold);
+        pial_mask=find(pial_metric>threshold);
+        wm_mask=find(wm_metric>threshold);
+        mapped_wm_mask=find(mapped_wm_metric>threshold);
     % Find vertices less than threshold
     else
         % Create pial and white maskss and mapped white mask
