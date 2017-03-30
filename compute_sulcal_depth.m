361196cdb5dd88fc5f941c63229a4897fcffb67c
jbonaiuto <jbonaiuto@gmail.com>
2017-03-30 11:25:20 +0100

    save hull surface

--- compute_sulcal_depth.m
+++ compute_sulcal_depth.m
@@ -1,4 +1,4 @@
-function [depth,HS]=compute_sulcal_depth(S)
+function [depth,HS]=compute_sulcal_depth(surface_file)
 % COMPUTE_SULCAL_DEPTH  Compute sulcal depth for each vertex in a surface
 %
 % Use as
@@ -9,8 +9,20 @@ function [depth,HS]=compute_sulcal_depth(S)
 % Requires cat toolbox for spm (http://www.neuro.uni-jena.de/cat/)
 
 spm('defaults','eeg');
-% Compute hull surface
-HS=cat_surf_fun('hull',S);
+S=gifti(surface_file);
+
+[path file ext]=fileparts(surface_file);
+hull_file=fullfile(path, sprintf('%s_hull.gii',file));
+if exist(hull_file,'file')~=2
+    % Compute hull surface
+    hull=cat_surf_fun('hull',S);
+    HS=gifti();
+    HS.vertices=hull.vertices;
+    HS.faces=hull.faces;
+    save(HS, hull_file);
+else
+    HS=gifti(hull_file);
+end
 % Compute mapping from surface to hull
 mapping=dsearchn(HS.vertices,S.vertices);
 % Compute distance for each vertex
