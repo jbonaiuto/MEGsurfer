1ed67fb75b34dfcf587f1d8569bb328b1d8588fb
FardinAFDIDEH (CRNL) <fardin.afdideh@gmail.com>
2019-02-01 18:28:11 +0100

    - dsearchn ---> knnsearch: faster implementation, - The second output of knnsearch() has been used to determine the distance between the corresponding vertices in the initial and decimated surfaces, - The percentage of the common vertices between the decimated and initial surfaces (for surface one) is displayed. The definition of 'common vertices' could be relaxed by including very close vertices (through comparing to a threshold): mean(~decim_orig_dist) ---> mean(~decim_orig_dist > Thr).

--- decimate_two_surfaces.m
+++ decimate_two_surfaces.m
@@ -8,7 +8,8 @@ FV.faces=surf1.faces;
 FV.vertices=surf1.vertices;
 FV_reduced=reducepatch(FV, ratio);
 
-orig_vert_idx=dsearchn(surf1.vertices,FV_reduced.vertices);
+[orig_vert_idx, decim_orig_dist] = knnsearch(surf1.vertices, FV_reduced.vertices);
+disp(['',num2str(mean(~decim_orig_dist)*100),'% of the vertices in the decimated first surface belong to the initial first surface vertices.'])
 
 write_surf_gifti(out_surf1, FV_reduced.vertices, FV_reduced.faces);
 write_surf_gifti(out_surf2, surf2.vertices(orig_vert_idx,:), FV_reduced.faces);
