3db9aaeb4023263268f13875751241837c117546
FardinAFDIDEH (CRNL) <fardin.afdideh@gmail.com>
2019-02-01 18:36:15 +0100

    Re-codes the part related to reducepatch() into one line.

--- decimate_two_surfaces.m
+++ decimate_two_surfaces.m
@@ -3,10 +3,7 @@ function decimate_two_surfaces(in_surf1, in_surf2, out_surf1, out_surf2, ratio)
 surf1=gifti(in_surf1);
 surf2=gifti(in_surf2);
 
-FV=[];
-FV.faces=surf1.faces;
-FV.vertices=surf1.vertices;
-FV_reduced=reducepatch(FV, ratio);
+FV_reduced = reducepatch(patch('Faces', surf1.faces, 'Vertices', surf1.vertices), ratio);
 
 [orig_vert_idx, decim_orig_dist] = knnsearch(surf1.vertices, FV_reduced.vertices);
 disp(['',num2str(mean(~decim_orig_dist)*100),'% of the vertices in the decimated first surface belong to the initial first surface vertices.'])
