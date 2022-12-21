cb89c15c3e5ca16bd76ccdc0b8c52a033b70884d
jbonaiuto <jbonaiuto@gmail.com>
2022-12-21 14:02:44 +0100

    updated comments

--- combine_surfaces.m
+++ combine_surfaces.m
@@ -1,9 +1,7 @@
 function combine_surfaces(surfaces, out_fname, varargin)
-% function combine_surfaces(surface_one_fname, surface_two_fname, out_fname, varargin)
 % Combines gifti surfaces into single gifti
 % INPUT: 
-%   surface_one_fname: surface filename
-%   surface_two_fname: surface fielname
+%   surfaces: cell array of surface filenames
 %   out_fname: new surface filename
 % ---------------------------
 % v1.0 James Bonaiuto (james.bonaiuto@isc.cnrs.fr)
@@ -12,16 +10,20 @@ function combine_surfaces(surfaces, out_fname, varargin)
 % Parse inputs
 defaults = struct();  %define default values
 params = struct(varargin{:});
-for f = fieldnames(defaults)',
-    if ~isfield(params, f{1}),
+for f = fieldnames(defaults)'
+    if ~isfield(params, f{1})
         params.(f{1}) = defaults.(f{1});
     end
 end
 
 % Read in each surface's gifti file
+% Have to re-index faces, this keeps track of current face index offset
 face_offset=0;
+
+% Load first surface
 mesh_one=gifti(surfaces{1});
-% Combine vertices and faces
+
+% Combine vertices, faces, and normal vectors
 combined_vertices=[mesh_one.vertices];
 combined_faces=[mesh_one.faces+face_offset];
 combined_normals=[];
@@ -31,17 +33,20 @@ end
 % mesh two face number will start with max mesh one face number+1
 face_offset=face_offset+size(mesh_one.vertices,1);
 
+% Add remaining surfaces
 for s=2:length(surfaces)
     mesh_two=gifti(surfaces{s});
 
-    % Combine vertices and faces
+    % Combine vertices, faces, and normal vectors
     combined_vertices=[combined_vertices; mesh_two.vertices];
     combined_faces=[combined_faces; mesh_two.faces+face_offset];
     if length(combined_normals)>0 && isfield(mesh_two,'normals')
         combined_normals=[combined_normals; mesh_two.normals];
     end
+    % Update face offset
     face_offset=face_offset+size(mesh_two.vertices,1);
 end
 
 % Create and save combined gifti
-write_surf_gifti(out_fname, combined_vertices, combined_faces, 'normals', combined_normals);
+write_surf_gifti(out_fname, combined_vertices, combined_faces, 'normals',...
+    combined_normals);
