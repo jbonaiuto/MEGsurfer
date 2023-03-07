affe4e1c9c180cb54d36516096777c1f8e2c1d4b
jbonaiuto <jbonaiuto@gmail.com>
2023-03-07 11:10:58 +0100

    generalized dir parameters

--- create_multilayer_surface.m
+++ create_multilayer_surface.m
@@ -1,7 +1,7 @@
-function create_multilayer_surface(subj_id, varargin)
+function create_multilayer_surface(subj_id, mri_path, surf_path)
 
 % Parse inputs
-defaults = struct('subjects_dir','/usr/local/freesurfer/subjects');  %define default values
+defaults = struct();  %define default values
 params = struct(varargin{:});
 for f = fieldnames(defaults)',
     if ~isfield(params, f{1}),
@@ -17,15 +17,14 @@ for l=1:length(layers)
     layer=layers(l);
     for h=1:length(hemispheres)
         hemi=hemispheres{h};
-        wm_file=fullfile(params.subjects_dir, subj_id, 'surf', sprintf('%s.white',hemi));
-        out_file=fullfile(params.subjects_dir, subj_id, 'surf', sprintf('%s.%.1f',hemi,layer));
+        wm_file=fullfile(surf_path, sprintf('%s.white',hemi));
+        out_file=fullfile(surf_path, sprintf('%s.%.1f',hemi,layer));
         [status, out]=system(['mris_expand -thickness ' wm_file ' ' sprintf('%.1f',layer) ' ' out_file]);        
     end
 end
 
 % Read RAS offset from freesurfer volume
-[status, out]=system(['mri_info --cras ' fullfile(params.subjects_dir,...
-    subj_id, 'mri', 'orig.mgz')]);
+[status, out]=system(['mri_info --cras ' fullfile(mri_path, 'orig.mgz')]);
 cols=strsplit(out,' ');
 ras_offset=[str2num(cols{1}) str2num(cols{2}) str2num(cols{3})]
 
@@ -34,10 +33,8 @@ for l=1:length(layers)
     layer=layers(l);
     for h_idx=1:length(hemispheres)    
         hemi=hemispheres{h_idx};
-        orig_name=fullfile(params.subjects_dir, subj_id,...
-            'surf', sprintf('%s.%.1f', hemi, layer));
-        new_name=fullfile(params.subjects_dir, subj_id,...
-            'surf', sprintf('%s.%.1f.gii', hemi, layer));
+        orig_name=fullfile(surf_path, sprintf('%s.%.1f', hemi, layer));
+        new_name=fullfile(surf_path, sprintf('%s.%.1f.gii', hemi, layer));
         system(sprintf('mris_convert %s %s', orig_name, new_name));
    
         % Read in each hemisphere's gifti file and adjust for RAS offset
@@ -51,25 +48,25 @@ for l=1:length(layers)
     end
     
     % combine hemispheres
-    lh=fullfile(params.subjects_dir, subj_id, 'surf', sprintf('lh.%.1f.gii', layer));
-    rh=fullfile(params.subjects_dir, subj_id, 'surf', sprintf('rh.%.1f.gii', layer));
-    combined=fullfile(params.subjects_dir, subj_id, 'surf', sprintf('%.1f.gii', layer));
+    lh=fullfile(surf_path sprintf('lh.%.1f.gii', layer));
+    rh=fullfile(surf_path, sprintf('rh.%.1f.gii', layer));
+    combined=fullfile(surf_path, sprintf('%.1f.gii', layer));
     combine_surfaces({lh, rh}, combined);
 end
 
 % downsample
-in_surfs={fullfile(params.subjects_dir, subj_id, 'surf', 'white.gii')};
-out_surfs={fullfile(params.subjects_dir, subj_id, 'surf', 'white.ds.gii')};
+in_surfs={fullfile(surf_path, 'white.gii')};
+out_surfs={fullfile(surf_path, 'white.ds.gii')};
 for l=1:length(layers)
     layer=layers(l);
-    in_surfs{end+1}=fullfile(params.subjects_dir, subj_id, 'surf', sprintf('%.1f.gii', layer));
-    out_surfs{end+1}=fullfile(params.subjects_dir, subj_id, 'surf', sprintf('%.1f.ds.gii', layer));
+    in_surfs{end+1}=fullfile(surf_path, sprintf('%.1f.gii', layer));
+    out_surfs{end+1}=fullfile(surf_path, sprintf('%.1f.ds.gii', layer));
 end
-in_surfs{end+1}=fullfile(params.subjects_dir, subj_id, 'surf', 'pial.gii');
-out_surfs{end+1}=fullfile(params.subjects_dir, subj_id, 'surf', 'pial.ds.gii');
+in_surfs{end+1}=fullfile(surf_path, 'pial.gii');
+out_surfs{end+1}=fullfile(surf_path, 'pial.ds.gii');
 decimate_multiple_surfaces(in_surfs, out_surfs, 0.1);
 
-combined=fullfile(params.subjects_dir, subj_id, 'surf', 'multilayer.ds.gii');
+combined=fullfile(surf_path, 'multilayer.ds.gii');
 % reverse order so surface order matches electrode order in laminar recordings
 out_surfs(end:-1:1) = out_surfs(:);
 combine_surfaces(out_surfs, combined);
