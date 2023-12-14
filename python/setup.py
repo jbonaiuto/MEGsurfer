d7afe1aaa1db71d4ec0c0828b3b5717bc4152a17
jbonaiuto <jbonaiuto@gmail.com>
2023-12-14 09:44:06 +0100

    moved to its own repository

--- python/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    name='csurf',
-    ext_modules=cythonize("csurf.pyx"),
-    zip_safe=False,
-)
--- python/util.py
+++
@@ -1,8 +0,0 @@
-import json
-
-
-def get_spm_path():
-    with open('settings.json') as settings_file:
-        parameters = json.load(settings_file)
-    spm_path = parameters["spm_path"]
-    return spm_path
